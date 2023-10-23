a83362588ec450c86abd9837a4d6805da8633b5a
Yeni Yazar Adı <yeniemail@example.com>
2023-10-23 21:02:55 +0300

    component parts are done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+
+const name = "Emir"
+const surname = "Bakar"
+const isLoggedIn = true
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h2>{name}</h2> */}
+      {/* <h2>{`Benim adım ${name}, soyadım ${surname}`}</h2> */}
+      <h2>{isLoggedIn ? `Benim adım ${name}, soyadım ${surname} ` : `Giriş yapmadınız!` } </h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
