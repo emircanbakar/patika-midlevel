34e31594ad27354bda6225588a409c7580b3cb5c
emircanbakar <emirbakar952@gmail.com>
2023-11-29 15:57:42 +0300

    contacts-app finished

--- contact-app/src/components/Contacts/index.js
+++ contact-app/src/components/Contacts/index.js
@@ -2,15 +2,27 @@ import React, { useEffect } from 'react'
 import List from "./List"
 import Form from "./Form"
 import { useState } from 'react'
+import "./style.css"
 
 function Contacts() {
-    const [contacts, setContacts] = useState([]);
+    const [contacts, setContacts] = useState([
+        {
+            fullname: "Mehmet",
+            phone_number: "12331"
+        },
+        {
+            fullname: "Ahmet",
+            phone_number: "14995" 
+        }
+
+    ]);
     useEffect(() =>{
         console.log(contacts)
     }, [contacts])
-
+    
     return (
-        <div>
+        <div id='container'>
+            <h2>Contacts</h2>
             <List contacts={contacts} />
             <Form addContact={setContacts} contacts={contacts} />
         </div>
