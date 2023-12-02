9b031c3fd21a8f32408fccb255ee749515c42327
Yeni Yazar Adı <yeniemail@example.com>
2023-12-03 02:04:40 +0300

    axios

--- fetch-lessons/src/components/Users.js
+++ fetch-lessons/src/components/Users.js
@@ -7,10 +7,9 @@ function Users() {
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((res) => res.json())
-            .then((data) =>
-                setUsers(data)
+        axios("https://jsonplaceholder.typicode.com/users")
+            .then((res) =>
+                setUsers(res.data)
             )
             .catch((e) => console.log(e))
             .finally(() => setIsLoading(false))
