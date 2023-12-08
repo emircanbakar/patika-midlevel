c862bacf8ccb251c6dd144ecf484772ceba907e7
Yeni Yazar Adı <yeniemail@example.com>
2023-12-08 16:26:32 +0300

    formik end

--- formik-lessons/src/components/SignUp.js
+++ formik-lessons/src/components/SignUp.js
@@ -3,7 +3,7 @@ import { useFormik } from 'formik';
 import validationSchema from "./validation"
 
 function SignUp() {
-    const { handleChange, handleSubmit, values } = useFormik({
+    const { handleChange, handleSubmit, handleBlur, values, errors, touched } = useFormik({
         initialValues: {
             email: '',
             password: "",
@@ -12,35 +12,48 @@ function SignUp() {
         onSubmit: values => {
             console.log(values);
         },
-        validationSchema 
+        validationSchema
     });
     return (
         <div>
             <h1>Sign Up</h1>
-
             <form onSubmit={handleSubmit}>
-
                 <label htmlFor="email">Email</label>
                 <input
                     name="email"
                     placeholder="jane@acme.com"
                     onChange={handleChange}
                     value={values.email}
+                    onBlur={handleBlur}
                 />
+                <br />
+                {
+                    errors.email && touched.email && (<div className='errors'> {errors.email} </div>)
+                }
                 <br />
                 <label >Password</label>
                 <input
                     name="password"
                     onChange={handleChange}
                     value={values.password}
+                    onBlur={handleBlur}
                 />
+                <br />
+                {
+                    errors.password &&  touched.password && (<div className='errors'> {errors.password} </div>)
+                }
                 <br />
                 <label>Password Again</label>
                 <input
                     name="passwordConfirmed"
                     onChange={handleChange}
                     value={values.passwordConfirmed}
+                    onBlur={handleBlur}
                 />
+                <br />
+                {
+                    errors.passwordConfirmed && touched.passwordConfirmed && (<div className='errors'> {errors.passwordConfirmed} </div>)
+                }
                 <br /><br />
                 <button type="submit">Submit</button>
                 <br /><br />
--- formik-lessons/src/components/validation.js
+++ formik-lessons/src/components/validation.js
@@ -1,9 +1,9 @@
-import { object, string, ref} from 'yup';
+import { object, string, ref, min} from 'yup';
 
 let validations = object({
-    email: string().email().required(),
-    password: string().required(),
-    passwordConfirmed: string().oneOf([ref('password')]).required(),
+    email: string().email("Geçerli bir email adresi giriniz").required("Zorunlu alanı doldurunuz!"),
+    password: string().required("Zorunlu alanı doldurunuz!").min(5, "Parolanız en az 5 karakter olmalıdır."),
+    passwordConfirmed: string().oneOf([ref('password')], "Parolanız eşleşmiyor!").required("Zorunlu alanı doldurunuz!"),
 });
 
 export default validations
