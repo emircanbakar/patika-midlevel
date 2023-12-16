7fdb264d0d6b47409c2827ad38657f777d8820b5
Yeni Yazar Adı <yeniemail@example.com>
2023-12-16 16:25:13 +0300

    realtime

---
+++ realtime/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Palette from './components/Palette';
+
+function App() {
+  return (
+    <div className="App">
+      <Palette></Palette>
+    </div>
+  );
+}
+
+export default App;
