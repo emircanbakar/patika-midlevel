a361a7e4be41b1e1e920aff0e95a342ed49a0949
emircanbakar <emirbakar952@gmail.com>
2023-12-19 15:17:28 +0300

    realtime apps w backend

--- realtime/src/App.js
+++ realtime/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { init, subs } from "./socketApi"
+import { useEffect, useState } from 'react';
 import Palette from './components/Palette';
 
 function App() {
+  const [activeColor, setActiveColor] = useState("#282c34")
+
+  useEffect(() => {
+    init()
+    subs((color) => {
+      setActiveColor(color)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <Palette></Palette>
+    <div className="App" style={{ backgroundColor: activeColor}}>
+      <Palette activeColor={activeColor} />
     </div>
   );
 }
