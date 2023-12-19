72fede51af35cf3dd60df8b7539322686fafe8d9
Yeni Yazar Adı <yeniemail@example.com>
2023-12-19 15:17:28 +0300

    realtime apps w backend

--- realtime/src/components/Palette.js
+++ realtime/src/components/Palette.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
+import { send } from "../socketApi"
 
-function Palette() {
+function Palette({activeColor}) {
+    const [color, setColor] = useState()
     return (
         <div className='palet'>
-            <input type="color" />
-            <button>Click</button>
+            <input
+                type="color"
+                value={activeColor}
+                onChange={(e) => setColor(e.target.value)}
+            />
+            <button onClick={() => send(color)}>Click</button>
+            {color}
         </div>
     )
 }
