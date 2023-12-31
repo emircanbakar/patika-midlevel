d2fedfbd3aeadb35b4a662b357619c587368d82a
Yeni Yazar Adı <yeniemail@example.com>
2023-12-31 05:24:21 +0300

    test denemeleri

---
+++ test-lessons/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from "./components/Counter"
+
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+      //app test ile çakışmaması için
+      <a href="">Learn React</a>
+    </div>
+  );
+}
+
+export default App;
