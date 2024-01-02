f73fab0dcc0d47e01ec3c78bcb5ce04b15c2ab60
Yeni Yazar Adı <yeniemail@example.com>
2024-01-02 04:58:46 +0300

    attıgım commitler bu hesaptan gitmemis... unluko

--- test-lessons/src/components/Todo/Todo.test.js
+++ test-lessons/src/components/Todo/Todo.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/valid-expect */
 import { act, fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Todo from "./index";
@@ -5,6 +6,7 @@ import Todo from "./index";
 describe("todo tests", () => {
   let button, input;
   beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
     render(<Todo />);
     button = screen.getByText("Add");
     input = screen.getByLabelText("Todo");
@@ -14,7 +16,9 @@ describe("todo tests", () => {
     expect(items.length).toEqual(3);
   });
   test("buton ve input olmalı", () => {
+    // eslint-disable-next-line no-unused-expressions, jest/valid-expect
     expect(button).toBeInTheDocument;
+    // eslint-disable-next-line no-unused-expressions
     expect(input).toBeInTheDocument;
   });
   test("Inputa string girilip butona basılınca listeye eklenmeli", () => {
