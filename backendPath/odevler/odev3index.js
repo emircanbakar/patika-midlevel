0b2c5bbd13e0a05b06d856c0cc2ddca0b0d710ed
emircanbakar <emirbakar952@gmail.com>
2024-11-22 04:23:01 +0300

    odev3

---
+++ backendPath/odevler/odev3index.js
@@ -0,0 +1,6 @@
+const { circleArea, circleCircumference } = require("./odev3");
+
+const radius = 5;
+
+console.log(`Dairenin Alanı: ${circleArea(radius)}`);
+console.log(`Dairenin Çevresi: ${circleCircumference(radius)}`);
