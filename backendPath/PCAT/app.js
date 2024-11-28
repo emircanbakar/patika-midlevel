9f3ad69c980968bce5f3eb49d2cad1dbcb5f0ca0
emircanbakar <emirbakar952@gmail.com>
2024-11-28 17:45:49 +0300

    mvc + update, delete

--- backendPath/PCAT/app.js
+++ backendPath/PCAT/app.js
@@ -3,6 +3,11 @@ const ejs = require('ejs');
 const path = require('path');
 const Photo = require('./modals/Photo');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const photoControllers = require('./controllers/photoControllers')
+const pageController = require('./controllers/pageController')
 
 const app = express();
 
@@ -14,29 +19,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
-  res.render('index', { photos });
-});
-app.get('/photos/:id', async (req, res) => {
-  //console.log(req.params.id)
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo,
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add', (req, res) => {
-  res.render('add');
-});
+app.get('/', photoControllers.getAllPhotos);
+app.get('/photos/:id', photoControllers.getOnePhoto);
+app.post('/photos', photoControllers.createPhoto);
+app.put('/photos/:id', photoControllers.updatePhoto);
+app.delete('/photos/:id', photoControllers.deletePhoto);
 
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
