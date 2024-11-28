da3b9f7ff0f92aa8c57edeacab15f5f763058523
Yeni Yazar Adı <yeniemail@example.com>
2024-11-28 07:28:53 +0300

    pcat projet + clean blog quiz update

--- backendPath/clean-blog/app.js
+++ backendPath/clean-blog/app.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost/clean-blog');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const addblog = await Post.find({});
+  res.render('index', { addblog });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add_post', (req, res) => {
   res.render('add_post');
+});
+
+app.post('/addblog', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const PORT = 3000;
---
+++ backendPath/clean-blog/models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  detail: { type: String, required: true },
+  dateCreated: { type: Date, default: Date.now },
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
