ad82ce931e2f2e443f61e5985bc156743926a04f
Yeni Yazar Adı <yeniemail@example.com>
2024-11-28 20:08:25 +0300

    pcat end

--- backendPath/clean-blog/app.js
+++ backendPath/clean-blog/app.js
@@ -2,39 +2,25 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const Post = require('./models/Post');
+const postController = require('./controllers/postController')
 
 const app = express();
-
 mongoose.connect('mongodb://localhost/clean-blog');
-
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-  const addblog = await Post.find({});
-  res.render('index', { addblog });
-});
-
-app.get('/addblog/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('post', {
-    post,
-  });
-});
+app.get('/', postController.getAllPosts);
+app.get('/addblog/:id', postController.getOnePost);
+app.post('/addblog', postController.createPost);
 
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add_post', (req, res) => {
   res.render('add_post');
-});
-
-app.post('/addblog', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
 });
 
 const PORT = 3000;
