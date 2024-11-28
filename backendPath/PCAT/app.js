16caaee3ccd97b013b01deb2666f7f4f0a9119ae
emircanbakar <emirbakar952@gmail.com>
2024-11-28 07:28:53 +0300

    pcat projet + clean blog quiz update

--- backendPath/PCAT/app.js
+++ backendPath/PCAT/app.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./modals/Photo');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 app.set('view engine', 'ejs');
 
 //middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', { photos });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
